857e7807ba89529f8cc7595af8588da691d24080
enzo agu <enzo.gustavo.agu@gmail.com>
2024-01-05 13:42:32 -0300

    actualización-carpeta-public

--- models/server.js
+++ models/server.js
@@ -36,7 +36,7 @@ class Server {
 
     this.app.use(cors());
     this.app.use(express.json());
-    this.app.use(express.static("./public"));
+    this.app.use(express.static("../public"));
     this.app.use(fileUpload({
       useTempFiles:true,
       tempFileDir:'/tmp',
