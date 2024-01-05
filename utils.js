f4213a849dc87fc74693bec6063a58f4f071da84
enzo agu <enzo.gustavo.agu@gmail.com>
2024-01-05 16:42:03 -0300

    sin-FILE-URL

--- utils.js
+++ utils.js
@@ -1,11 +1,11 @@
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import dotenv from 'dotenv'
+// import path, { dirname } from 'path'
+// import { fileURLToPath } from 'url'
+// import dotenv from 'dotenv'
 
-export const __fileName=fileURLToPath(import.meta.url)
-export const __dirName=path.dirname(__fileName)
+// export const __fileName=fileURLToPath(import.meta.url)
+// export const __dirName=path.dirname(__fileName)
 
-export function dotenvfunction (){
+// export function dotenvfunction (){
 
-    dotenv.config({ path: "../.env" });
-}
+//     dotenv.config({ path: "../.env" });
+// }
